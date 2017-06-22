8547f479984243709e6367f5d24f1c879808df1e
yohohone <yohoho_ne@mail.ru>
2017-06-22 16:28:56 +0300

    create Calculate

--- calculate.java
+++ calculate.java
@@ -2,6 +2,16 @@ public class calculate {
 
 	public static void main(String [] arg) {
 		System.out.println("Calculate...");
-	
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		int minus = first - second;
+		float adv = first / second;
+		float sqr = first * first;
+		System.out.println("Sum " + sum);
+		System.out.println("Minus " + minus);
+		System.out.println("Delenie " + adv);
+		System.out.println("Kvadrat 1go " + sqr);	
+
 	}
 }
